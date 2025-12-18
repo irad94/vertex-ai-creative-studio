cbcc51e9d0872865c07ab74a57015aac1e05f20e
irad94 <irad.olriv@gmail.com>
2025-12-17 21:11:53 -0600

    feat: agregar validaciones de variables de entorno en el script de construcción

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-gcloud builds submit . --project=$PROJECT_ID --gcs-source-staging-dir=gs://run-resources-$PROJECT_ID-$REGION/services/creative-studio --region=$REGION --service-account=projects/$PROJECT_ID/serviceAccounts/builds-creative-studio@$PROJECT_ID.iam.gserviceaccount.com
+$ErrorActionPreference = "Stop"
+
+# Asegúrate de que las variables de entorno estén definidas
+if (-not $env:PROJECT_ID) { throw "La variable de entorno PROJECT_ID no está definida." }
+if (-not $env:REGION) { throw "La variable de entorno REGION no está definida." }
+
+gcloud builds submit . `
+    --project=$env:PROJECT_ID `
+    --gcs-source-staging-dir="gs://run-resources-$($env:PROJECT_ID)-$($env:REGION)/services/creative-studio" `
+    --region=$env:REGION `
+    --service-account="projects/$($env:PROJECT_ID)/serviceAccounts/builds-creative-studio@$($env:PROJECT_ID).iam.gserviceaccount.com"
